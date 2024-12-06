410d46d800699f0dc5011b95dffb0effde2ad2aa
Ashton Kemerling <ashton.kemerling@protonmail.com>
2024-12-05 20:43:46 -0700

    Day five done.

--- src/day_five/mod.rs
+++ src/day_five/mod.rs
@@ -1,13 +1,15 @@
-use std::{collections::{HashMap, HashSet}, str::FromStr};
-
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 pub fn solve(input: String) {
     println!("Simple solution: {}", solve_simple(&input));
+    println!("Fixed solution: {}", solve_corrected(&input));
 }
 
-
 pub fn solve_simple(input: &str) -> usize {
-    let mut orderings: Vec<Ordering> =  Vec::new();
+    let mut orderings: Vec<Ordering> = Vec::new();
     let mut sum = 0;
     let mut parse_ordering = true;
     for line in input.lines() {
@@ -16,7 +18,10 @@ pub fn solve_simple(input: &str) -> usize {
         } else if parse_ordering {
             orderings.push(line.parse().expect("Should be able to parse ordering"));
         } else {
-            let numbers: Vec<usize> =  line.split(",").map(|c| c.parse().expect("Should have numbers")).collect();
+            let numbers: Vec<usize> = line
+                .split(",")
+                .map(|c| c.parse().expect("Should have numbers"))
+                .collect();
             if check_ordering(&numbers, &orderings) {
                 sum += numbers.get((numbers.len() - 1) / 2).unwrap_or(&0);
             }
@@ -25,10 +30,32 @@ pub fn solve_simple(input: &str) -> usize {
     sum
 }
 
+pub fn solve_corrected(input: &str) -> usize {
+    let mut orderings: Vec<Ordering> = Vec::new();
+    let mut sum = 0;
+    let mut parse_ordering = true;
+    for line in input.lines() {
+        if line.is_empty() {
+            parse_ordering = false;
+        } else if parse_ordering {
+            orderings.push(line.parse().expect("Should be able to parse ordering"));
+        } else {
+            let numbers: Vec<usize> = line
+                .split(",")
+                .map(|c| c.parse().expect("Should have numbers"))
+                .collect();
+            if !check_ordering(&numbers, &orderings) {
+                let fixed = correct_ordering(numbers.as_slice(), &orderings);
+                sum += fixed.get((fixed.len() - 1) / 2).unwrap_or(&0);
+            }
+        }
+    }
+    sum
+}
 
 struct Ordering {
     before: usize,
-    after: usize
+    after: usize,
 }
 
 #[derive(Debug)]
@@ -38,8 +65,7 @@ impl FromStr for Ordering {
     type Err = ParseOrderingError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (a, b) = s.split_once("|")
-            .ok_or(ParseOrderingError)?;
+        let (a, b) = s.split_once("|").ok_or(ParseOrderingError)?;
 
         let a_fromstr = a.parse::<usize>().map_err(|_| ParseOrderingError)?;
         let b_fromstr = b.parse::<usize>().map_err(|_| ParseOrderingError)?;
@@ -56,17 +82,65 @@ fn check_ordering(pages: &[usize], ordering: &[Ordering]) -> bool {
     for order in ordering {
         let mut default = HashSet::new();
         default.insert(order.after);
-        exclusions.entry(order.before).and_modify(|s| {s.insert(order.after);}).or_insert(default);
+        exclusions
+            .entry(order.before)
+            .and_modify(|s| {
+                s.insert(order.after);
+            })
+            .or_insert(default);
     }
-        pages.iter()
+    pages
+        .iter()
         .map(|page| {
-            let result = exclusions.get(page)
+            let result = exclusions
+                .get(page)
                 .map(|set| set.intersection(&seen).collect::<Vec<&usize>>().is_empty())
                 .unwrap_or(true);
             seen.insert(*page);
             result
         })
         .all(|x| x)
+}
+
+fn correct_ordering(pages: &[usize], orderings: &[Ordering]) -> Vec<usize> {
+    let mut result = Vec::new();
+    let mut exclusions: HashMap<usize, HashSet<usize>> = HashMap::new();
+    let mut seen: HashSet<usize> = HashSet::new();
+    for order in orderings {
+        let mut default = HashSet::new();
+        default.insert(order.after);
+        exclusions
+            .entry(order.before)
+            .and_modify(|s| {
+                s.insert(order.after);
+            })
+            .or_insert(default);
+    }
+    for page in pages {
+        let intersection: Vec<&usize> = exclusions
+            .get(page)
+            .map(|s| s.intersection(&seen).collect())
+            .unwrap_or_default();
+        if intersection.is_empty() {
+            result.push(*page);
+        } else {
+            let min_index: usize = intersection
+                .iter()
+                .map(|val| {
+                    result
+                        .iter()
+                        .position(|f| f == *val)
+                        .expect("Should have an index")
+                })
+                .min()
+                .unwrap_or(result.len());
+
+            result.insert(min_index, *page);
+        }
+        seen.insert(*page);
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -76,29 +150,195 @@ mod tests {
     #[test]
     fn test_ordering() {
         let orderings = [
-            Ordering { before: 47, after: 53 },
-            Ordering { before: 97, after: 13 },
-            Ordering { before: 97, after: 61 },
-            Ordering { before: 97, after: 47 },
-            Ordering { before: 75, after: 29 },
-            Ordering { before: 61, after: 13 },
-            Ordering { before: 75, after: 53 },
-            Ordering { before: 29, after: 13 },
-            Ordering { before: 97, after: 29 },
-            Ordering { before: 53, after: 29 },
-            Ordering { before: 61, after: 53 },
-            Ordering { before: 97, after: 53 },
-            Ordering { before: 61, after: 29 },
-            Ordering { before: 47, after: 13 },
-            Ordering { before: 75, after: 47 },
-            Ordering { before: 97, after: 75 },
-            Ordering { before: 47, after: 61 },
-            Ordering { before: 75, after: 61 },
-            Ordering { before: 47, after: 29 },
-            Ordering { before: 75, after: 13 },
-            Ordering { before: 53, after: 13 },
+            Ordering {
+                before: 47,
+                after: 53,
+            },
+            Ordering {
+                before: 97,
+                after: 13,
+            },
+            Ordering {
+                before: 97,
+                after: 61,
+            },
+            Ordering {
+                before: 97,
+                after: 47,
+            },
+            Ordering {
+                before: 75,
+                after: 29,
+            },
+            Ordering {
+                before: 61,
+                after: 13,
+            },
+            Ordering {
+                before: 75,
+                after: 53,
+            },
+            Ordering {
+                before: 29,
+                after: 13,
+            },
+            Ordering {
+                before: 97,
+                after: 29,
+            },
+            Ordering {
+                before: 53,
+                after: 29,
+            },
+            Ordering {
+                before: 61,
+                after: 53,
+            },
+            Ordering {
+                before: 97,
+                after: 53,
+            },
+            Ordering {
+                before: 61,
+                after: 29,
+            },
+            Ordering {
+                before: 47,
+                after: 13,
+            },
+            Ordering {
+                before: 75,
+                after: 47,
+            },
+            Ordering {
+                before: 97,
+                after: 75,
+            },
+            Ordering {
+                before: 47,
+                after: 61,
+            },
+            Ordering {
+                before: 75,
+                after: 61,
+            },
+            Ordering {
+                before: 47,
+                after: 29,
+            },
+            Ordering {
+                before: 75,
+                after: 13,
+            },
+            Ordering {
+                before: 53,
+                after: 13,
+            },
         ];
-        assert!(check_ordering(&[75,47,61,53,29], &orderings));
-        assert!(!check_ordering(&[75,97,47,61,53], &orderings));
+        assert!(check_ordering(&[75, 47, 61, 53, 29], &orderings));
+        assert!(!check_ordering(&[75, 97, 47, 61, 53], &orderings));
+    }
+
+    #[test]
+    fn test_correct_ordering() {
+        let orderings = [
+            Ordering {
+                before: 47,
+                after: 53,
+            },
+            Ordering {
+                before: 97,
+                after: 13,
+            },
+            Ordering {
+                before: 97,
+                after: 61,
+            },
+            Ordering {
+                before: 97,
+                after: 47,
+            },
+            Ordering {
+                before: 75,
+                after: 29,
+            },
+            Ordering {
+                before: 61,
+                after: 13,
+            },
+            Ordering {
+                before: 75,
+                after: 53,
+            },
+            Ordering {
+                before: 29,
+                after: 13,
+            },
+            Ordering {
+                before: 97,
+                after: 29,
+            },
+            Ordering {
+                before: 53,
+                after: 29,
+            },
+            Ordering {
+                before: 61,
+                after: 53,
+            },
+            Ordering {
+                before: 97,
+                after: 53,
+            },
+            Ordering {
+                before: 61,
+                after: 29,
+            },
+            Ordering {
+                before: 47,
+                after: 13,
+            },
+            Ordering {
+                before: 75,
+                after: 47,
+            },
+            Ordering {
+                before: 97,
+                after: 75,
+            },
+            Ordering {
+                before: 47,
+                after: 61,
+            },
+            Ordering {
+                before: 75,
+                after: 61,
+            },
+            Ordering {
+                before: 47,
+                after: 29,
+            },
+            Ordering {
+                before: 75,
+                after: 13,
+            },
+            Ordering {
+                before: 53,
+                after: 13,
+            },
+        ];
+
+        assert_eq!(
+            vec![61, 29, 13],
+            correct_ordering(&[61, 13, 29], &orderings)
+        );
+        assert_eq!(
+            vec![97, 75, 47, 61, 53],
+            correct_ordering(&[75, 97, 47, 61, 53], &orderings)
+        );
+        assert_eq!(
+            vec![97, 75, 47, 29, 13],
+            correct_ordering(&[97, 13, 75, 29, 47], &orderings)
+        );
     }
 }
